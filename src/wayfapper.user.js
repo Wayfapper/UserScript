7d4a7a0759cc3fe539f82fb034a4eb25fc402135
Falk Rund <Un1matr1x@users.noreply.github.com>
2020-12-04 20:05:23 +0100

    add version 0.0.12
    for the record

--- src/wayfapper.user.js
+++ src/wayfapper.user.js
@@ -2,7 +2,7 @@
 // @id				wayfapper
 // @name			Wayfapper
 // @category		Misc
-// @version			0.0.10
+// @version			0.0.12
 // @description		WAYFArer + mAPPER = Wayfapper
 // @namespace		https://wfp.cr4.me/
 // @downloadURL		https://wfp.cr4.me/dl/wayfapper.user.js
@@ -40,7 +40,7 @@
         return token;
     }
 
-    const WEBHOOK_URL = 'https://wfp.cr4.me/api/v4/webhook.php';
+    const WEBHOOK_URL = 'https://wfp.cr4.me/api/v5/webhook.php';
     const WEBHOOK_TOKEN = await getToken();
 
     /**
@@ -51,6 +51,151 @@
     if (window.location.href.indexOf('.ingress.com/intel') > -1) {
         console.log('[WFP]: intel ingress recognized');
 
+        var seenGuids = new Set();
+        var portals = [];
+        var inFlight = false;
+        var timerStarted = false;
+
+        var sendData = function (data) {
+            fetch(WEBHOOK_URL + "?&p=w&t="+ WEBHOOK_TOKEN, {
+                method: 'POST',
+                body: data
+            }).then(function (response) {
+                if (!response.ok) {
+                    if (response.status >= 400 && response.status <= 499) {
+                        console.error("portalfinder: 4xx error, not retrying", response);
+                    } else {
+                    }
+                } else {
+                    inFlight = false;
+                    checkSend();
+                }
+            }).catch(function (error) {
+                console.warn("portalfinder: network error, retrying in 10 seconds", error);
+                window.setTimeout(function () {
+                    sendData(data);
+                }, 10000);
+            });
+        };
+
+        var checkSend = function () {
+            if (timerStarted || inFlight) {
+                return;
+            }
+
+            if (Object.keys(portals).length !== 0) {
+                timerStarted = true;
+                window.setTimeout(function () {
+                    timerStarted = false;
+                    inFlight = true;
+                    sendData(JSON.stringify(portals));
+                    portals = [];
+                }, 3000);
+            }
+        };
+
+        var cleanSeenSet = function () {
+            seenGuids.clear();
+        };
+
+        var safeTrim = function (str) {
+            if (typeof str === 'string') {
+                return str.trim();
+            } else {
+                return str;
+            }
+        };
+
+        var IPortal = function (guid, ent) {
+            return [
+                guid,
+                ent[2]/1E6, // latE6
+                ent[3]/1E6, // lngE6
+                ent[8], // title
+                safeTrim(ent[7]) // image
+            ];
+        };
+
+        var ingressMethodRegex = /^(?:(?:https?:)?\/\/(?:www\.|intel\.)?ingress\.com)?\/r\/(getPortalDetails|getEntities)$/i;
+        (function (open) {
+            XMLHttpRequest.prototype.open = function () {
+                if (window.disable_portalfinder) {
+                    // Testing override
+                    open.apply(this, arguments);
+                    return;
+                }
+
+                var apiFunc, match;
+                if ((match = arguments[1].match(ingressMethodRegex)) !== null) {
+                    apiFunc = match[1];
+                    var getPortalDetailsGuid;
+                    if (apiFunc === 'getPortalDetails') {
+                        var origSend = this.send;
+                        this.send = function () {
+                            getPortalDetailsGuid = JSON.parse(arguments[0]).guid;
+                            origSend.apply(this, arguments);
+                        };
+                    }
+                    this.addEventListener("readystatechange", function () {
+                        if (this.readyState === 4 && this.status === 200) {
+                            try {
+                                if ((this.responseText === '{}') || (this.responseText.startsWith('<!DOCTYPE html>'))) {
+                                    return;
+                                }
+
+                                var data;
+                                switch (apiFunc) {
+                                    case 'getPortalDetails':
+                                        var guid = getPortalDetailsGuid;
+                                        if (!seenGuids.has(guid)) {
+                                            seenGuids.add(guid);
+                                            data = JSON.parse(this.responseText);
+                                            if (data.result === undefined) {
+                                                return;
+                                            }
+                                            portals.push(new IPortal(guid, data.result));
+                                        }
+                                        break;
+                                    case 'getEntities':
+                                        data = JSON.parse(this.responseText);
+                                        if ((data.result === undefined) || (data.result.map === undefined)) {
+                                            return;
+                                        }
+
+                                        for (var tile in data.result.map) {
+                                            if (data.result.map.hasOwnProperty(tile)) {
+                                                if (data.result.map[tile].gameEntities === undefined) {
+                                                    continue;
+                                                }
+
+                                                data.result.map[tile].gameEntities.forEach(function (ent) {
+                                                    switch (ent[2][0]) { // Entity type
+                                                        case 'p': // Portal
+                                                            var guid = ent[0];
+                                                            if (!seenGuids.has(guid)) {
+                                                                cleanSeenSet();
+                                                                seenGuids.add(guid);
+                                                                portals.push(new IPortal(guid, ent[2]));
+                                                            }
+                                                            break;
+                                                    }
+                                                });
+                                            }
+                                        }
+                                        break;
+                                    default:
+                                        return;
+                                }
+                                checkSend();
+                            } catch (e) {
+                                console.error("portalfinder: Caught error in Intel XHR hook", apiFunc, e, this.responseText);
+                            }
+                        }
+                    }, false);
+                }
+                open.apply(this, arguments);
+            };
+        })(XMLHttpRequest.prototype.open);
 
         //Check for Wayfapper
     } else if (window.location.href.indexOf('wfp.cr4.me') > -1) {
