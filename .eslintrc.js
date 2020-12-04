676de899f6761e15c9279a6f24cacf3f500e6735
Falk Rund <Un1matr1x@users.noreply.github.com>
2020-12-04 20:21:55 +0100

    fix lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,18 @@
 module.exports = {
   env: {
-    'browser': true,
-    'es6': true,
+    browser: true,
+    es6: true,
   },
   extends: [
-    'google',
-    'plugin:prettier/recommended'
+    "google", "plugin:prettier/recommended"
   ],
   globals: {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
-  rules: {
-  },
+  rules: {},
 };
