7c147234629f2a725045c2ca020cde6f8ab435c5
Falk Rund <Un1matr1x@users.noreply.github.com>
2020-12-05 08:52:36 +0100

    Update wayfapper.user.js
    add first visible hint for wayfapper script to wayfarer

--- src/wayfapper.user.js
+++ src/wayfapper.user.js
@@ -47,14 +47,65 @@
   // define basic parameter that are used everywhere
   const WEBHOOK_URL = "https://wfp.cr4.me/api/v5/webhook.php";
   const WEBHOOK_TOKEN = await getToken();
+
+  /**
+   * Add some stylerules to wayfarer
+   * @param none
+   * @return none
+   */
+  function addWayfarerCss () {
+    const css = "
+      a.glyphicon.glyphicon-share {
+        margin-right: 13px;
+      }
+      a.sidebar__item.sidebar-wayfapper {
+        padding-left: 15px;
+      }
+      a.sidebar__item.sidebar-wayfapper:hover {
+        padding-left: 10px;
+      }
+      span.fapper {
+        font-family: Akkurat,Roboto,sans-serif;
+      }
+      .badge {
+        position: absolute;
+        padding: 5px 10px !important;
+        border-radius: 50%;
+        background-color: rgba(0, 0, 0, 0.5);
+        color: white;
+      }";
+    const style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = css;
+    document.querySelector("head").appendChild(style);
+  }
+
+  /**
+   * Add some visible representation of wayfapper to wayfarer
+   * @param none
+   * @return none
+   */
+  function addWayfarerVisibles () {
+    const badge_date = document.createElement('span');
+    badge_date.innerHTML = '&nbsp;';
+    badge_date.id = 'bage_hover';
+    badge_date.className = 'badge';
+    const badge = document.querySelector('.sidebar__item--settings span').parentNode;
+    badge.insertBefore(badge_date, badge.childNodes[0]);
+  }
+
   if (window.location.href.indexOf("wfp.cr4.me") > -1) {
     console.log("[WFP]: Wayfapper recognized");
+    console.log("[WFP]: API: " + WEBHOOK_URL);
+    console.log("[WFP]: Token: " + WEBHOOK_TOKEN);
     // TODO add stuff here, later
   } else if (window.location.href.indexOf(".ingress.com/intel") > -1) {
     console.log("[WFP]: Ingress Intel-Map recognized");
     // TODO restore intel functions here
   } else if (window.location.href.indexOf("wayfarer.nianticlabs.com") > -1) {
     console.log("[WFP]: Wayfarer recognized");
+    addWayfarerCss();
+    addWayfarerVisibles();
     // TODO restore wayfarer functions here
   } else {
     console.log("[WFP]: pages mismatch");
