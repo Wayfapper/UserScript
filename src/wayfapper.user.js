45b09f5c498e2ba88016dd959a1ba13aaff60ca6
Falk Rund <Un1matr1x@users.noreply.github.com>
2020-12-05 08:14:49 +0100

    Update wayfapper.user.js
    Add basic functions for page recognition

--- src/wayfapper.user.js
+++ src/wayfapper.user.js
@@ -33,4 +33,31 @@
    * Overall script parts are placed here
    */
   console.log("[WFP]: init");
+
+  // Token is stored via GM_get/setValue, we need async functions here
+  async function getToken(a) {
+    let token = await GM.getValue("wayfapper-token", -1);
+    return token;
+  }
+
+  //define basic parameter that are used everywhere
+  const WEBHOOK_URL = 'https://wfp.cr4.me/api/v5/webhook.php';
+  const WEBHOOK_TOKEN = await getToken();
+
+  if (window.location.href.indexOf('wfp.cr4.me') > -1) {
+    console.log('[WFP]: Wayfapper recognized');
+    // TODO add stuff here, later
+
+  } else if (window.location.href.indexOf('.ingress.com/intel') > -1) {
+    console.log('[WFP]: Ingress Intel-Map recognized');
+    // TODO restore intel functions here
+
+  } else if (window.location.href.indexOf('wayfarer.nianticlabs.com') > -1) {
+    console.log('[WFP]: Wayfarer recognized');
+    // TODO restore wayfarer functions here
+
+  } else {
+    console.log('[WFP]: pages mismatch');
+    console.log('[WFP]: ' + window.location.href);
+  }
 })();
