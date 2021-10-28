e34b51c3e3b123386c7a988f7ab5d20ef75c80a6
Falk Rund <Un1matr1x@users.noreply.github.com>
2021-10-28 15:10:00 +0200

    [dev] raise version for development Bump version to 0.2.13

--- src/wayfapper.user.js
+++ src/wayfapper.user.js
@@ -2,7 +2,7 @@
 // @id              wayfapper
 // @name            Wayfapper
 // @category        Misc
-// @version         0.2.12
+// @version         0.2.13
 // @description     WAYFArer + mAPPER = Wayfapper
 // @namespace       https://wfp.cr4.me/
 // @downloadURL     https://wfp.cr4.me/dl/wayfapper.user.js
