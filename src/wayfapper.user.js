4849f16d24339158b1d156494aa69cacf820facb
Falk Rund <Un1matr1x@users.noreply.github.com>
2021-12-11 15:05:31 +0100

    [dev] raise version for development Bump version to 0.2.17

--- src/wayfapper.user.js
+++ src/wayfapper.user.js
@@ -2,7 +2,7 @@
 // @id              wayfapper
 // @name            Wayfapper
 // @category        Misc
-// @version         0.2.16
+// @version         0.2.17
 // @description     WAYFArer + mAPPER = Wayfapper
 // @namespace       https://wfp.cr4.me/
 // @downloadURL     https://wfp.cr4.me/dl/wayfapper.user.js
@@ -34,7 +34,7 @@
    * Overall script parts are placed here
    * and define basic parameter here, we wan't to use everywhere
    */
-  const DEBUG = false;
+  const DEBUG = true;
   const logPrefix = "[WFP_" + GM_info.script.version + "]: ";
   const WEBHOOK_URL = "https://wfp.cr4.me/api/v3/webhook.php";
   const WEBHOOK_TOKEN = await getToken();
